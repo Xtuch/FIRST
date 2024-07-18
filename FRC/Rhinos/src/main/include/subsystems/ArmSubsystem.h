8d09a1c176e0460e85855aa8683b5184816fd9c9
Xtuch <123428189+Xtuch@users.noreply.github.com>
2024-07-17 18:58:37 -0600

    Create ArmSubsystem.h

---
+++ FRC/Rhinos/src/main/include/subsystems/ArmSubsystem.h
@@ -0,0 +1,62 @@
+// Copyright (c) Paolo Reyes for Rhinos.
+
+#pragma once
+
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <frc/controller/PIDController.h>
+#include <frc/controller/ArmFeedforward.h>
+#include <frc/DutyCycleEncoder.h>
+#include <frc2/command/SubsystemBase.h>
+
+#include "Constants.h"
+
+class ArmSubsystem : public frc2::SubsystemBase {
+ public:
+    // Arm constructor
+    ArmSubsystem(void);
+
+    /**
+     * Will be called periodically whenever the CommandScheduler runs.
+     */
+    void Periodic() override;
+
+    // Subsystem methods go here.
+
+    /**
+     * Drives the robot at given x, y and theta speeds. Speeds range from [-1, 1]
+     * and the linear speeds have no effect on the angular speed.
+     *
+     * @param desiredAngle Angle to move the arm to.
+     */
+    void MoveToAngle(double desiredAngle);
+
+    /**
+    * Locks the arm at the current angle.
+    */
+    void LockAngle(bool intakePositon, bool outakePosition, bool shootingPosition, bool bButton);
+
+    /**
+     * Returns the heading of the robot.
+     *
+     * @return the robot's heading in degrees, from 180 to 180
+     */
+    double GetAngle(void);
+
+    void waitForAngle(double angle);
+
+    void resetIntegralError();
+
+ private:
+   ctre::phoenix6::hardware::TalonFX m_armMotor1;
+   ctre::phoenix6::hardware::TalonFX m_armMotor2;
+   double setpoint;
+   double previous_time;
+   frc::DutyCycleEncoder s_armAbsoluteEncoder;
+
+   double pidOutput;
+   double error;
+   double integral_error;
+   units::volt_t motorsOutput;
+
+   double transToUnitCircle(double angle);
+};
