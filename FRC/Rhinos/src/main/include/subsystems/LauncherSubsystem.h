5c3faad1c06872ca75d6c7c4c7689969024a6b37
Xtuch <123428189+Xtuch@users.noreply.github.com>
2024-07-17 19:00:32 -0600

    Create LauncherSubsystem.h

---
+++ FRC/Rhinos/src/main/include/subsystems/LauncherSubsystem.h
@@ -0,0 +1,48 @@
+
+// Copyright (c) Paolo Reyes for Rhinos.
+
+#pragma once
+
+#include "ShooterSubsystem.h"
+#include "IntakeSubsystem.h"
+#include <frc2/command/SubsystemBase.h>
+
+#include <frc/Timer.h>
+
+#include "Constants.h"
+
+class LauncherSubsystem : public frc2::SubsystemBase {
+ public:
+    // Launcher constructor
+    LauncherSubsystem(void);
+
+    /**
+     * Will be called periodically whenever the CommandScheduler runs.
+     */
+    void Periodic() override;
+
+    // Subsystem methods go here.
+
+     /**
+     * Sets the speed of the shooter motors in RPM
+     */
+    void shootAtSpeed(double speed);
+
+    /**
+     * Reads the triggers and enables intaking and outaking
+     */
+    void intakeAndOutake(double speed1, double speed2, bool shoot);
+
+    void autonomousShoot();
+
+    void intakeForSeconds(double seconds);
+
+    void resetIntegralError(void);
+
+ private:
+    ShooterSubsystem m_shooter;
+    IntakeSubsystem m_intake;
+
+    double at_speed_counts = 0;
+    double waitTime = LauncherConstants::kDelayBeforeShooting;
+};
